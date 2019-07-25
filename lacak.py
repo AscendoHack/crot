e1b34ba36fe36ce721ee52a901a6dae6756dd705
AscendoHack <52598832+AscendoHack@users.noreply.github.com>
2019-07-25 23:01:49 +0700

    Add files via upload

---
+++ lacak.py
@@ -0,0 +1,85 @@
+from sys import *
+from time import *
+import os
+import time
+
+def mengetik(n):
+	    for i in n + '\n':
+		stdout.write(i)
+		stdout.flush()
+		sleep(0.2)
+os.system('clear')
+print "\033[1;31m   _        _    ____    _    _  __   ___  ____      _    _   _  ____"
+print "  | |      / \  / ___|  / \  | |/ /  / _ \|  _ \    / \  | \ | |/ ___|"
+print "  | |     / _ \| |     / _ \ | ' /  | | | | |_) |  / _ \ |  \| | |  _"
+print "  | |___ / ___ \ |___ / ___ \| . \  | |_| |  _ <  / ___ \| |\  | |_| |"
+print "  |_____/_/   \_\____/_/   \_\_|\_\  \___/|_| \_\/_/   \_\_| \_|\____|"
+time.sleep(3)
+print
+print
+print
+print " [*] Code By ENTUP-HACK "
+time.sleep(1)
+print " [*] Github https://github.com/AscendoHack"
+time.sleep(1)
+print " [*] Youtube = ENTUP ID"
+time.sleep(1)
+print " [*] JANGAN DIJUAL BELI KAN !!!"
+time.sleep(1)
+print
+mengetik("\033[1;34mMASUKAN NAMA KORBAN..")
+time.sleep(1)
+print "@root->"
+pilih = raw_input("    root@localhost(NAMA) -> ")
+time.sleep(1)
+print "[*] Checking username",pilih,"on:"
+time.sleep(2)
+print "[+] Instagram:  Found! https://www.instagram.com/",pilih,""
+print "[+] Facebook:  Found! https://www.facebook.com/",pilih,""
+print "[+] Twitter:  Found! https://www.twitter.com/",pilih,""
+print "[+] Blogger:  Found! https://",pilih,".blogspot.com"
+print "[+] GooglePlus:  Found! https://plus.google.com/+",pilih,"/posts"
+print "[+] Reddit:  Found! https://www.reddit.com/user/",pilih,""
+print "[+] Wordpress:  Found! https://",pilih,".wordpress.com"
+print "[+] Pinterest:  Found! https://www.pinterest.com/",pilih,""
+print "[+] Github:  Found! https://www.github.com/",pilih,""
+print "[+] Tumblr:  Found! https://",pilih,".tumblr.com"
+print "[+] Flickr:  Found! https://www.flickr.com/photos/",pilih,""
+print "[+] Steam:  Found! https://steamcommunity.com/id/",pilih,""
+print "[+] Vimeo:  Found! https://vimeo.com/",pilih,""
+print "[+] SoundCloud:  Found! https://soundcloud.com/",pilih,""
+print "[+] Disqus:  Found! https://disqus.com/",pilih,""
+print "[+] Medium:  Found! https://medium.com/@",pilih,""
+print "[+] DeviantART:  Found! https://",pilih,".deviantart.com"
+print "[+] VK:  Found! https://vk.com/",pilih,""
+print "[+] About.me:  Found! https://about.me/",pilih,""
+print "[+] Imgur:  Found! https://imgur.com/user/",pilih,""
+print "[+] Flipboard:  Found! https://flipboard.com/@",pilih,""
+print "[+] SlideShare:  Found! https://slideshare.net/",pilih,""
+print "[+] Fotolog:  Found! https://fotolog.com/",pilih,""
+print "[+] Spotify:  Found! https://open.spotify.com/user/",pilih,""
+print "[+] MixCloud:  Found! https://www.mixcloud.com/",pilih,""
+print "[+] Scribd:  Found! https://www.scribd.com/",pilih,""
+print "[+] Patreon:  Found! https://www.patreon.com/",pilih,""
+print "[+] BitBucket:  Found! https://bitbucket.org/",pilih,""
+print "[+] DailyMotion:  Found! https://www.dailymotion.com/",pilih,""
+print "[+] CshMe:  Found! https://cash.me/",pilih,""
+print "[+] Behance:  Found! https://www.behance.net/",pilih,""
+print "[+] GoodReads:  Found! https://www.goodreads.com/",pilih,""
+print "[+] Instructables:  Found! https://www.instructables.com/member/",pilih,""
+print "[+] Keybase:  Found! https://keybase.io/",pilih,""
+print "[+] Kongregate:  Found! https://kongregate.com/accounts/",pilih,""
+print "[+] LiveJournal:  Found! https://",pilih,".livejournal.com"
+print "[+] AngelList:  Found! https://angel.co/",pilih,""
+print "[+] last.fm:  Found! https://last.fm/user/",pilih,""
+print "[+] Dribbble:  Found! https://dribbble.com/",pilih,""
+print "[+] Codecademy:  Found! https://www.codecademy.com/",pilih,""
+print "[+] Gravatar:  Found! https://en.gravatar.com/",pilih,""
+print "[+] Pastebin:  Found! https://pastebin.com/u/",pilih,""
+print "[+] Roblox:  Found! https://foursquare.com/",pilih,""
+print "[+] Newgrounds:  Found! https://",pilih,".newgrounds.com"
+print "[+] Wattpad:  Found! https://www.wattpad.com/user/",pilih,""
+print "[+] Canva:  Found! https://www.canva.com/",pilih,""
+print "[+] CreativeMarket:  Found! https://creativemarket.com/",pilih,""
+print "[+] Trakt:  Found! https://www.trakt.tv/users/",pilih,""
+os.system('xdg-open https://www.youtube.com/channel/UCXygmpynwtyPudwtzM46Cfg')
